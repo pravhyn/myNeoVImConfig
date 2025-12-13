162ce916c1c60e98f31845b85a436d79e186d7cb
praveenscript <praveenkumarjkr@gmail.com>
2025-12-13 21:05:31 +0530

    nothing

--- lua/plugins/rosepinemoon.lua
+++ lua/plugins/rosepinemoon.lua
@@ -1,22 +1,23 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	priority = 1000, -- Load early to apply colors before plugins
-	lazy = false,
-	config = function()
-		require("rose-pine").setup({
-			variant = "moon", -- Options: "auto", "main", "moon", "dawn"
-			dark_variant = "moon",
-			dim_inactive_windows = true,
-			extend_background_behind_borders = true,
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000, -- Load early to apply colors before plugins
+        lazy = false,
+        config = function()
+                require("rose-pine").setup({
+                        variant = "moon", -- Options: "auto", "main", "moon", "dawn"
+                        dark_variant = "moon",
+                        dim_inactive_windows = true,
+                        extend_background_behind_borders = true,
 
-			styles = {
-				bold = true,
-				italic = false,
-				transparency = true, -- set to true if using a transparent terminal
-			},
-		})
+                        styles = {
+                                bold = true,
+                                italic = false,
+                                transparency = true, -- set to true if using a transparent terminal
+                        },
+                })
 
-		vim.cmd("colorscheme rose-pine")
-	end,
+                vim.cmd("colorscheme rose-pine")
+        end,
+        -- { "rebelot/kanagawa.nvim" },
 }
