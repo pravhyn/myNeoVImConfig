fe881a43f51b8c3b6330c29e576edf7bf23fb4f9
praveenscript <praveenkumarjkr@gmail.com>
2025-12-13 21:05:10 +0530

    nothing

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,8 @@
-
-
 return {
-  "nvim-lualine/lualine.nvim",
-  enabled = not _G.is_termux,
-  config = function()
-    -- print("[Lualine] Loaded (is_termux: " .. tostring(_G.is_termux) .. ")")
-    require("core.lualine").setup()
-  end,
+        "nvim-lualine/lualine.nvim",
+        enabled = not _G.is_termux,
+        config = function()
+                -- print("[Lualine] Loaded (is_termux: " .. tostring(_G.is_termux) .. ")")
+                require("core.lualine").setup()
+        end,
 }
